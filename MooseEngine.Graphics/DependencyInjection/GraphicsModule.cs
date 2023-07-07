5252ac5892544cb9e1838da8a1b84e0b774b5335
KingoBoiii <kingodk@outlook.dk>
2023-07-07 09:50:01 +0200

    Added MooseEngine.Graphics project

---
+++ MooseEngine.Graphics/DependencyInjection/GraphicsModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace MooseEngine.Extensions.DependencyInjection;
+
+public class GraphicsModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterModule<OpenGLGraphicsModule>();
+    }
+}

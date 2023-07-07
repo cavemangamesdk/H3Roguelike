543268a081064bb7b9179a5bfee6f9585d837b8e
KingoBoiii <kingodk@outlook.dk>
2023-07-07 09:50:13 +0200

    Added new MooseEngine project

---
+++ MooseEngine/ApplicationBase.cs
@@ -0,0 +1,51 @@
+﻿using MooseEngine.Graphics;
+
+namespace MooseEngine;
+
+public interface IApplication
+{
+}
+
+internal interface IExecutableApplication : IDisposable
+{
+    void Initialize();
+    void Run();
+}
+
+public abstract class ApplicationBase : IApplication, IExecutableApplication
+{
+    public ApplicationBase(IWindow window, IRenderer renderer)
+    {
+        Window = window;
+        Renderer = renderer;
+    }
+
+    private IWindow Window { get; }
+    private IRenderer Renderer { get; }
+
+    public void Initialize()
+    {
+        Window.Initialize();
+
+        InitializeApplication();
+    }
+
+    protected virtual void InitializeApplication()
+    {
+    }
+
+    public virtual void Run()
+    {
+        while(!Window.ShouldClose)
+        {
+            Window.Update();
+
+            Renderer.Clear();
+        }
+    }
+
+    public void Dispose()
+    {
+        Window.Dispose();
+    }
+}
---
+++ MooseEngine/Engine.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using MooseEngine.Extensions.DependencyInjection;
+
+namespace MooseEngine;
+
+public static class Engine
+{
+    public static void Start<TApplication>()
+        where TApplication : ApplicationBase
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.RegisterType<TApplication>()
+            .AsImplementedInterfaces()
+            .SingleInstance();
+
+        containerBuilder.RegisterModule<PlatformModule>();
+        containerBuilder.RegisterModule<GraphicsModule>();
+
+        var container = containerBuilder.Build();
+
+        using var scope = container.BeginLifetimeScope();
+
+        using var application = scope.Resolve<IExecutableApplication>();
+
+        application.Initialize();
+
+        application.Run();
+    }
+}

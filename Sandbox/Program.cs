ed1d104b76193d15cb1afb799298ecd7bf625562
KingoBoiii <kingodk@outlook.dk>
2023-07-07 09:50:23 +0200

    Added Sandbox project to test new engine

---
+++ Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using MooseEngine;
+using MooseEngine.Graphics;
+
+Engine.Start<SandboxApplication>();
+
+class SandboxApplication : ApplicationBase
+{
+    public SandboxApplication(IWindow window, IRenderer renderer)
+        : base(window, renderer)
+    {
+    }
+}

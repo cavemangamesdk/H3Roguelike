8adf8433bdc0e33679ab83eb7191ebf405be1e43
KingoBoiii <kingodk@outlook.dk>
2023-07-08 07:46:03 +0200

    Added GettingStarted_Textures layer

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -17,6 +17,27 @@ class SandboxApplication : ApplicationBase
 
     protected override void InitializeApplication()
     {
-        AttachLayer(new GettingStarted_HelloTriangleLayer(Renderer, GraphicsFactory));
+        //AttachLayer(new GettingStarted_HelloTriangleLayer(Renderer, GraphicsFactory));
+        AttachLayer(new GettingStarted_TexturesLayer());
+    }
+}
+
+class GettingStarted_TexturesLayer : LayerBase
+{
+    public GettingStarted_TexturesLayer()
+        : base("LearnOpenGL - GettingStarted-Textures")
+    {
+    }
+
+    public override void OnAttach()
+    {
+    }
+
+    public override void OnDetach()
+    {
+    }
+
+    public override void Update(float deltaTime)
+    {
     }
 }

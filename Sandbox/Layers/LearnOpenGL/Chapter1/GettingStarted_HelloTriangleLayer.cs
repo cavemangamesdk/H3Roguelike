77bfa876267ae35c2145c431f3036472194d33c4
KingoBoiii <kingodk@outlook.dk>
2023-07-08 07:44:29 +0200

    Moved GettingStarted_HelloTriangle chapter into its own layer

---
+++ Sandbox/Layers/LearnOpenGL/Chapter1/GettingStarted_HelloTriangleLayer.cs
@@ -0,0 +1,63 @@
+﻿using MooseEngine;
+using MooseEngine.Graphics;
+
+class GettingStarted_HelloTriangleLayer : LayerBase
+{
+    public GettingStarted_HelloTriangleLayer(IRenderer renderer, IGraphicsFactory graphicsFactory) 
+        : base("LearnOpenGL - GettingStarted-HelloTriangle")
+    {
+        Renderer = renderer;
+        GraphicsFactory = graphicsFactory;
+    }
+
+    private IRenderer Renderer { get; }
+    private IGraphicsFactory GraphicsFactory { get; }
+
+    // Runtime
+    private IPipeline? Pipeline { get; set; }
+    private IVertexBuffer? VertexBuffer { get; set; }
+    private IIndexBuffer? IndexBuffer { get; set; }
+
+    public override void OnAttach()
+    {
+        var shader = GraphicsFactory.CreateShader();
+
+        var bufferLayout = new BufferLayout(new List<BufferElement>
+        {
+            new BufferElement("aPos", ShaderDataType.Float3)
+        });
+        Pipeline = GraphicsFactory.CreatePipeline(shader, bufferLayout);
+
+        var vertices = new float[4 * 3] {
+             0.5f,  0.5f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left 
+        };
+
+        //var vertices = new float[3 * 3] {
+        //    -0.5f, -0.5f, 0.0f,
+        //     0.5f, -0.5f, 0.0f,
+        //     0.0f,  0.5f, 0.0f
+        //};
+
+        VertexBuffer = GraphicsFactory.CreateVertexBuffer(vertices);
+
+        var indices = new uint[] {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+        };
+        IndexBuffer = GraphicsFactory.CreateIndexBuffer(indices);
+    }
+
+    public override void OnDetach()
+    {
+    }
+
+    public override void Update(float deltaTime)
+    {
+        Renderer.Clear();
+
+        Renderer.DrawGeometry(Pipeline, VertexBuffer, IndexBuffer);
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -17,67 +17,6 @@ class SandboxApplication : ApplicationBase
 
     protected override void InitializeApplication()
     {
-        AttachLayer(new SandboxLayer(Renderer, GraphicsFactory));
-    }
-}
-
-class SandboxLayer : LayerBase
-{
-    public SandboxLayer(IRenderer renderer, IGraphicsFactory graphicsFactory) 
-        : base("Sandbox Layer")
-    {
-        Renderer = renderer;
-        GraphicsFactory = graphicsFactory;
-    }
-
-    private IRenderer Renderer { get; }
-    private IGraphicsFactory GraphicsFactory { get; }
-
-    // Runtime
-    private IPipeline? Pipeline { get; set; }
-    private IVertexBuffer? VertexBuffer { get; set; }
-    private IIndexBuffer? IndexBuffer { get; set; }
-
-    public override void OnAttach()
-    {
-        var shader = GraphicsFactory.CreateShader();
-
-        var bufferLayout = new BufferLayout(new List<BufferElement>
-        {
-            new BufferElement("aPos", ShaderDataType.Float3)
-        });
-        Pipeline = GraphicsFactory.CreatePipeline(shader, bufferLayout);
-
-        var vertices = new float[4 * 3] {
-             0.5f,  0.5f, 0.0f,  // top right
-             0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left 
-        };
-
-        //var vertices = new float[3 * 3] {
-        //    -0.5f, -0.5f, 0.0f,
-        //     0.5f, -0.5f, 0.0f,
-        //     0.0f,  0.5f, 0.0f
-        //};
-
-        VertexBuffer = GraphicsFactory.CreateVertexBuffer(vertices);
-
-        var indices = new uint[] {  // note that we start from 0!
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
-        };
-        IndexBuffer = GraphicsFactory.CreateIndexBuffer(indices);
-    }
-
-    public override void OnDetach()
-    {
-    }
-
-    public override void Update(float deltaTime)
-    {
-        Renderer.Clear();
-
-        Renderer.DrawGeometry(Pipeline, VertexBuffer, IndexBuffer);
+        AttachLayer(new GettingStarted_HelloTriangleLayer(Renderer, GraphicsFactory));
     }
 }

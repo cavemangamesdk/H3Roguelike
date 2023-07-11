6b876eb9252ea5afb47c15681707a4ed46c3d9b3
KingoBoiii <kingodk@outlook.dk>
2023-07-11 08:58:09 +0200

    Added Roguelike Test layer

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -46,6 +46,47 @@ class SandboxApplication : ApplicationBase
         //AttachLayer(new GettingStarted_TexturesLayer(Renderer, GraphicsFactory));
         //AttachLayer(new CameraTestLayer(Window, Renderer, GraphicsFactory));
         //AttachLayer(new Renderer2DTestLayer(Renderer, Renderer2D, GraphicsFactory));
-        AttachLayer(new SceneTestLayer(Renderer, Renderer2D, GraphicsFactory, SceneFactory));
+        //AttachLayer(new SceneTestLayer(Renderer, Renderer2D, GraphicsFactory, SceneFactory));
+        AttachLayer(new RoguelikeTestLayer(Renderer, Renderer2D, GraphicsFactory, SceneFactory));
+    }
+}
+
+class RoguelikeTestLayer : LayerBase
+{
+    public RoguelikeTestLayer(IRenderer renderer, IRenderer2D renderer2D, IGraphicsFactory graphicsFactory, ISceneFactory sceneFactory) 
+        : base("Roguelike Test Layer")
+    {
+        Renderer = renderer;
+        Renderer2D = renderer2D;
+        GraphicsFactory = graphicsFactory;
+        SceneFactory = sceneFactory;
+    }
+
+    private IRenderer Renderer { get; }
+    private IRenderer2D Renderer2D { get; }
+    private IGraphicsFactory GraphicsFactory { get; }
+    private ISceneFactory SceneFactory { get; }
+
+    // Runtime
+    private IScene? Scene { get; set; }
+
+    public override void OnAttach()
+    {
+        Renderer2D.Initialize();
+
+        Scene = SceneFactory.CreateScene();
+        Scene?.OnRuntimeStart();
+    }
+
+    public override void OnDetach()
+    {
+        Scene?.OnRuntimeStop();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        Renderer.Clear();
+
+        Scene?.OnRuntimeUpdate(deltaTime);
     }
 }
